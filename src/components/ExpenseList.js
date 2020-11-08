2c571759599787b123fe168632c522fc52e56b0b
Anudeep NV <venkataanudeepnayani@gmail.com>
2020-11-08 15:58:23 +0530

    Setup formatting for amount and createdAt

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import ExpenseListItem from './ExpenseListItem'
-import selectedExpenses from '../selectors/expenses'
+import selectExpenses from '../selectors/expenses'
 
 export const ExpenseList = (props) => (
     <div>
@@ -9,17 +9,17 @@ export const ExpenseList = (props) => (
             props.expenses.length === 0 ? (
                 <p>No expenses</p>
             ) : (
-                props.expenses.map((expense) => {
-                    return <ExpenseListItem key={expense.id} {...expense} />
-                })
-            )
+                    props.expenses.map((expense) => {
+                        return <ExpenseListItem key={expense.id} {...expense} />
+                    })
+                )
         }
     </div>
 )
 
 const mapStateToProps = (state) => {
     return {
-        expenses: selectedExpenses(state.expenses, state.filters)
+        expenses: selectExpenses(state.expenses, state.filters)
     }
 }
 
